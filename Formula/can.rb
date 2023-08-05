d906386778e68b8470ed975c734eb4a91fe4c524
Tshaka Lekholoane <mail+git@tshaka.dev>
2023-08-05 19:09:39 +0200

    feat: update can tap

--- Formula/can.rb
+++ Formula/can.rb
@@ -1,8 +1,8 @@
 class Can < Formula
   desc "macOS command-line utility that provides an alternative to the rm command"
   homepage "https://tshaka.dev/x/can"
-  url "https://github.com/tshakalekholoane/can/archive/refs/tags/0.1.1.tar.gz"
-  sha256 "e281571c3a51fb2b4e2717cb2f6e9ef905c92a10fbc25bc1d9c1635223301897"
+  url "https://github.com/tshakalekholoane/can/archive/refs/tags/0.2.0.tar.gz"
+  sha256 "040739d75c024e848297bc8a17c85a265a10467cd4633ac4dc627e20c39ba34d"
   license "ISC"
 
   def install
